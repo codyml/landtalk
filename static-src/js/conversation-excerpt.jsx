5dc33fad02d2e0473ff4aaf5958d65932c3e3a47
Cody M Leff <mail@codymleff.com>
2019-09-10 16:43:16 -0700

    Updated conversation card style to match reference; cleaned up package.json (closes #59).

--- static-src/js/conversation-excerpt.jsx
+++ static-src/js/conversation-excerpt.jsx
@@ -33,7 +33,7 @@ const ConversationExcerpt = ({ conversation }) => {
                 <div className='card-image image is-3by2' style={{ backgroundImage: `url('${ conversation.historical_image_url }')` }}></div>
                 <div className='card-content'>
                     <div className='is-size-5 has-text-weight-light'>{ conversation.place_name }</div>
-                    <div className='content' dangerouslySetInnerHTML={ { __html: summary } }></div>
+                    <div className='card-summary'>{ summary }</div>
                 </div>
             </div>
         </a>
